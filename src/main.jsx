d8c5bc43e7f153828d142c212e4a3a873ef41e0c
Amaury Lesigne <lesigne.amaury@gmail.com>
2026-01-29 09:47:42 +0100

    fix for deploying

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { AuthWrapper } from "./context/AuthContext.jsx";
-import "./app.css";
+import "./App.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
